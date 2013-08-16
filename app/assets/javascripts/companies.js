41a5f8b8d0e2a398bd8f2712120928d490c51d9e
MakerSquare Student <students@themakersquare.com>
2013-08-16 13:11:40 -0500

    added click and scroll over jquery to index page

--- app/assets/javascripts/companies.js
+++ app/assets/javascripts/companies.js
@@ -1,14 +1,24 @@
-
-
 $(document).ready(function(){
+  $('.block').hide();
+
   $('.company').mouseenter(function(){
     $(this).css("font-weight", "bold");
   }).mouseleave(function(){
     $(this).css("font-weight", "normal");
   });
-  $('.company').click(function(){
-    $(this).next().slideToggle();
+
+// / Hides-shows the company logo and info, called 'block' class, on mouse hover:
+  // $('.title').click(function(){
+  //   $(this).next().slideToggle();
+  // });
+
+// Hides-shows the company logo and info, called 'block' class, on mouse enter and leave:
+ $('.company').mouseenter(function(){
+    $(this).children().show('.block');
+  }).mouseleave(function(){
+    $('.block').hide();
   });
+
 });
 
 
