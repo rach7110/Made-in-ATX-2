2ea5b1fb44116f22c34ae768a9150255122fbb4c
Rachel Loziuk <rachel.loziuk@gmail.com>
2013-09-26 11:56:44 -0500

    added company overview; fixed positioning of info div; added link paths to edit and delete

--- app/assets/javascripts/companies.js
+++ app/assets/javascripts/companies.js
@@ -2,17 +2,17 @@ $(document).ready(function(){
   // Pageload: Hides the company info block to start
   $('.block').hide();
 
-// Mouse hover: Bolds the info block
-  // $('.company').mouseenter(function(){
-  //   $(this).css("font-weight", "bold");
-  // }).mouseleave(function(){
-  //   $(this).css("font-weight", "normal");
-  // });
+// // Mouse hover: Bolds the info block
+//   // $('.company').mouseenter(function(){
+//   //   $(this).css("font-weight", "bold");
+//   // }).mouseleave(function(){
+//   //   $(this).css("font-weight", "normal");
+//   // });
 
-// / Mouse click: Hides-shows the company logo and info, called 'block' class
-  // $('.title').click(function(){
-  //   $(this).next().slideToggle();
-  // });
+// // / Mouse click: Hides-shows the company logo and info, called 'block' class
+//   // $('.title').click(function(){
+//   //   $(this).next().slideToggle();
+//   // });
 
 // Mouse enter/leave: Hides-shows the company logo and info, called 'block' class
  $('.company').mouseenter(function(){
