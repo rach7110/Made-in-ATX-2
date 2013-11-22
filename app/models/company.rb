8859fab43a0c1612c746d60f4b03bef742c0fbbf
Rachel Loziuk <rachel.loziuk@gmail.com>
2013-11-21 18:16:02 -0600

    moved API data pull requests to companies model

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,11 +1,23 @@
+
+require 'open-uri'
+require 'json'
+
 class Company < ActiveRecord::Base
   attr_accessible :name, :category_code, :homepage_url, :overview, :image
   # validates_presence_of :name
 
-
+		#-------Crunchbase::API.key = "bxqwjskgyrg2hjykcrc6dbpy"
+	json_file = JSON.parse(open("http://api.crunchbase.com/v/1/search.js?geo=78701&range=15&page=1&api_key=bxqwjskgyrg2hjykcrc6dbpy").read)
+	crunchbase_companies = json_file["results"] 
+	    #-------Store API data into the DB:
+	if @company.present? == false
+		crunchbase_companies.each do |crunchbase_company|
+  		@company = Company.create(:name => crunchbase_company["name"], :category_code => crunchbase_company["category_code"], :homepage_url => crunchbase_company["homepage_url"], :overview => crunchbase_company["overview"], :image => crunchbase_company["image"])
+    end
+  end
 end
 
-
+	
 # Crunchbase API data format (the 'results' key is an array containing a hash for each company:
 # Example:
 # "name" => "Skylist"  (***)
